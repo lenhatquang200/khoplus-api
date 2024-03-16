5c03941cb730103e0fa809d78ea6150a3860edcd
Q <nhatquang2106qh@gmail.com>
2024-03-16 09:37:48 +0700

    add cors3

--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
 define('LARAVEL_START', microtime(true));
 define('STDIN',fopen("php://stdin","r"));
 
