f4cc359d8f398aa6384ebc76d8f358d339610e6b
Long <sprhlong@gmail.com>
2024-03-25 12:31:57 +0700

    fix required gender trong model Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -107,7 +107,7 @@ use Illuminate\Database\Eloquent\Model;
         'ward_id' => 'nullable',
         'note' => 'nullable|string|max:191',
         'image' => 'nullable|string|max:191',
-        'gender' => 'nullable',
+        'gender' => 'required',
         'created_at' => 'nullable',
         'updated_at' => 'nullable'
     ];
